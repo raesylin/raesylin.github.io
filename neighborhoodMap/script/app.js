8a1051f805082f77a98915e3c939d9357053c261
raesylin <raesylin@gmail.com>
2015-07-28 18:08:39 -0700

    Fixed KO bindings for crime map

--- neighborhoodMap/script/app.js
+++ neighborhoodMap/script/app.js
@@ -1,13 +1,12 @@
-function init() {
-	var map;
-	// var geocoder;
-	initialize();
-}
+var initialSetting = {
+	latlng: new google.maps.LatLng(32.857738, -117.21151),
+	zoom: 8
+};
 
-function initialize() {
-	var mapOptions = {
-		zoom: 8,
-		center : new google.maps.LatLng(32.857738, -117.21151),
+var setOpt = function(zoom, latlng) {
+	var mapOptions = {		
+		zoom: zoom,
+		center : latlng,
 		panControl: false,
 		zoomControl: true,
 		mapTypeControl: true,
@@ -15,57 +14,169 @@ function initialize() {
 		streetViewControl: true,
 		overviewMapControl: false,
 	};
+	return mapOptions;
+};
 
-	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-	// geocoder = new google.maps.Geocoder();
-}
+var stylesArray = [
+	{
+		featureType: 'all',
+		stylers: [
+			{saturation: -80}
+		]
+	},{
+		featureType: 'road.arterial',
+		elementType: 'geometry',
+		stylers: [
+			{hue: "#00ffee"},
+			{saturation: 50}
+		]
+	},{
+		featureType: 'poi.business',
+		elementType: 'labels',
+		stylers: [
+			{visibility: "off"}
+		]
+	}
+];
 
-function codeAddress() {
-	var geocoder = new google.maps.Geocoder();
-	var $address = $('#address').val();
-	geocoder.geocode({'address': $address}, function(results, status) {
-		if (status == google.maps.GeocoderStatus.OK) {
-			map.setCenter(results[0].geometry.location);
-			map.setOptions({zoom: 15});
-			var marker = new google.maps.Marker({
-				map: map,
-				position: results[0].geometry.location
-			});
-		} else {
-			alert('Geocode was not successful for the following reason: ' + status);
-		};
+var myLatLng = function(location) {
+	this.lat = location.lat();
+	this.lng = location.lng();
+};
+
+var getDateRange = function(range) {
+
+	var todayInSec = Date.now();
+	var end = new Date(todayInSec);
+	var start = new Date(todayInSec - range * 24 * 3600 * 1000);
+
+	var dateRange = {
+		startDate: (start.getMonth()+1).toString() + '/' + start.getDate().toString() + '/' + start.getFullYear().toString(),
+		endDate: (end.getMonth()+1).toString() + '/' + end.getDate().toString() + '/' + end.getFullYear().toString()
+	};
+	return dateRange;
+};
+
+var processCrimeLoc = function(data) {
+	var crimeData = [];
+	for (var i = 0; i < data.length; i++) {
+		crimeData.push(new google.maps.LatLng(data[i].lat, data[i].long));
+	};
+	return crimeData;
+};
+
+var mapViewModel = function() {
+	var self = this;
+
+	self.showInfo = ko.observable(false);
+	self.togglePane = ko.observable(false);
+
+	self.address = ko.observable();
+	self.dateDelta = ko.observable('7');
+	self.dateDelta.subscribe(function() {
+		self.setCrimeMap();
 	});
-}
+
+	self.crimeMapButton = ko.observable(false);
+	self.coordinate = ko.observable();
+
+	var initializeMap = function(setting) {
+		var mapOptions = new setOpt(setting.zoom, setting.latlng);
+		self.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+		self.coordinate(setting.latlng);
+		self.heatmap = new google.maps.visualization.HeatmapLayer();
+	};
+
+	// Operations:
+	// -- Initialize map
+
+	initializeMap(initialSetting);
+
+	// -- Center map to new address
+
+	this.newHome = function() {
+		self.showInfo(false);
+		self.crimeMapButton(false);
+		self.heatmap.setMap(null);
+
+		var geocoder = new google.maps.Geocoder();
+
+		geocoder.geocode({'address': self.address()}, function(results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				self.coordinate(results[0].geometry.location);
+				self.map.setCenter(self.coordinate());
+				self.map.setOptions({zoom: 14});
+				self.homeMarker = new google.maps.Marker({
+					map: self.map,
+					position: self.coordinate(),
+					title: "Your new home",
+				});
+				if (self.heatmap) {
+					self.heatmap.setMap(null);
+				};
+				self.togglePane(true);
+				self.setCrimeMap();
+			} else {
+				alert('Geocode was not successful for the following reason: ' + status);
+			};
+		});
+
+	};
+
+	this.toggleCrimeMap = function() {
+		self.heatmap.setMap(self.heatmap.getMap() ? null : self.map);
+		self.map.setOptions(self.heatmap.getMap() ? {styles: stylesArray} : {styles: []});
+	};
+
+	this.setCrimeMap = function() {
+		// self.togglePane(true);
+		var range_int = parseInt(self.dateDelta());
+		var dateRange = getDateRange(range_int);
+		this.startDate = dateRange.startDate;
+		this.endDate = dateRange.endDate;
+		
+		$.ajax({
+			url: 'https://jgentes-Crime-Data-v1.p.mashape.com/crime?enddate='+this.endDate+'&lat='+self.coordinate().lat()+'&long='+self.coordinate().lng()+'&startdate='+this.startDate,
+			type: 'GET',
+			dataType: 'json',
+			success: function(data) {
+				console.log(data.length);
+				if (data.length != 0) {
+					var crimeData = processCrimeLoc(data);
+					var pointArray = new google.maps.MVCArray(crimeData);
+					if (self.heatmap) {
+						self.heatmap.setData(pointArray);
+					} else {
+						self.heatmap = new google.maps.visualization.HeatmapLayer({
+	    					data: pointArray
+						});
+					} ;
+					self.crimeMapButton(true);
+					// self.heatmap.setMap(self.map);
+				} else {
+					self.showInfo(true);
+				};
+			},
+			error: function(err) { alert(err) ;},
+			beforeSend: function(xhr) {
+				xhr.setRequestHeader("X-Mashape-Authorization", "KqEmEEqnUomsh36Fd17Aei0l9fGqp1BTzhCjsnMPd4iZC218H6");
+			}
+		});
+	};
 
 
-// function loadScript() {
-// 	var script = document.createElement('script');
-// 	script.type = 'text/javascript';
-// 	script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' + 
-// 		'&signed_in=true&callback=init';
-// 	document.body.appendChild(script);
-// }
 
-// window.onload = loadScript;
+	// Get new center after moving map and request new crime data
+	google.maps.event.addListener(self.map, 'dragend', function() {
+		self.coordinate(self.map.getCenter());
+		self.setCrimeMap();
+		// window.setTimeout(function() {
 
-google.maps.event.addDomListener(window, 'load', initialize);
-
-function crimeLog() {
-	$.ajax({
-	    url: 'https://jgentes-Crime-Data-v1.p.mashape.com/crime?enddate=6%2F25%2F2015&lat=32.715&long=-117.1625&startdate=6%2F19%2F2014',
-	    type: 'GET', // The HTTP Method
-	    data: {}, // Additional parameters here
-	    datatype: 'json',
-	    success: function(data) { 
-	    	for (i in data) {
-	    		console.log(data[i].description); 
-	    	};
-	    },
-	    error: function(err) { alert(err); },
-	    beforeSend: function(xhr) {
-	    xhr.setRequestHeader("X-Mashape-Authorization", "KqEmEEqnUomsh36Fd17Aei0l9fGqp1BTzhCjsnMPd4iZC218H6"); // Enter here your Mashape key
-    	}
+		// }, 500);
 	});
-}
 
-// window.onload = crimeLog;
+};
+
+$(document).ready(function() {
+	ko.applyBindings(new mapViewModel);
+});
--- neighborhoodMap/script/test.js
+++ neighborhoodMap/script/test.js
@@ -1,12 +1,175 @@
-$.ajax({
-	url: 'https://jgentes-Crime-Data-v1.p.mashape.com/crime?enddate=enddate=6%2F25%2F2015&lat=32.85&long=-117.21&startdate=6%2F19%2F2014',
-	type: 'GET',
-	dataType: 'json',
-	success: function(data) { 
-		console.log(); 
-	},
-	error: function(err) { alert(err) ;},
-	beforeSend: function(xhr) {
-		xhr.setRequestHeader("X-Mashape-Authorization", "a5axdWoV2Xmshmoa205omjGF3NZop14QCAsjsnQN5GoFAOSzBl");
-	}
+var initialSetting = {
+	latlng: new google.maps.LatLng(32.857738, -117.21151),
+	zoom: 8
+};
+
+var setOpt = function(zoom, latlng) {
+	var mapOptions = {		
+		zoom: zoom,
+		center : latlng,
+		panControl: false,
+		zoomControl: true,
+		mapTypeControl: true,
+		scaleControl: true,
+		streetViewControl: true,
+		overviewMapControl: false,
+	};
+	return mapOptions;
+};
+
+var myLatLng = function(location) {
+	this.lat = location.lat();
+	this.lng = location.lng();
+};
+
+var getDateRange = function(range) {
+
+	var todayInSec = Date.now();
+	var end = new Date(todayInSec);
+	var start = new Date(todayInSec - range * 24 * 3600 * 1000);
+
+	var dateRange = {
+		startDate: (start.getMonth()+1).toString() + '/' + start.getDate().toString() + '/' + start.getFullYear().toString(),
+		endDate: (end.getMonth()+1).toString() + '/' + end.getDate().toString() + '/' + end.getFullYear().toString()
+	};
+	return dateRange;
+};
+
+var processCrimeLoc = function(data) {
+	var crimeData = [];
+	for (var i = 0; i < data.length; i++) {
+		crimeData.push(new google.maps.LatLng(data[i].lat, data[i].long));
+	};
+	return crimeData;
+};
+
+var mapViewModel = function() {
+	var self = this;
+
+	var $info = $('#info');
+	
+	self.address = ko.observable();
+	self.dateDelta = ko.observable('7');
+
+	var initializeMap = function(setting) {
+		var mapOptions = new setOpt(setting.zoom, setting.latlng);
+		self.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+		self.coordinate = setting.latlng;
+	};
+
+	// Operations:
+
+	// -- Initialize map
+	initializeMap(initialSetting);
+
+	// -- Center map to new address
+	this.newHome = function() {
+		console.log(self.heatmap);
+		var geocoder = new google.maps.Geocoder();
+		// var $address = $(form).children('#address').val();
+
+		geocoder.geocode({'address': self.address()}, function(results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				self.coordinate = results[0].geometry.location;
+				self.map.setCenter(self.coordinate);
+				self.map.setOptions({zoom: 14});
+				self.homeMarker = new google.maps.Marker({
+					map: self.map,
+					position: self.coordinate,
+					title: "Your new home",
+				});
+				if (self.heatmap) {
+					console.log('resetting heatmap...');
+					console.log(self.heatmap);
+					self.heatmap.setMap(null);
+				};
+				// self.testCrimeMap(self.coordinate);
+			} else {
+				alert('Geocode was not successful for the following reason: ' + status);
+			};
+		});
+
+	};
+
+	// 
+	// Crime map
+	// 
+
+	this.testCrimeMap = function(coordinate) {
+		var range_int = parseInt(self.dateDelta());
+		var dateRange = getDateRange(range_int);
+		this.startDate = dateRange.startDate;
+		this.endDate = dateRange.endDate;
+		
+		$.ajax({
+			url: 'https://jgentes-Crime-Data-v1.p.mashape.com/crime?enddate='+this.endDate+'&lat='+coordinate.lat()+'&long='+coordinate.lng()+'&startdate='+this.startDate,
+			type: 'GET',
+			dataType: 'json',
+			success: function(data) {
+				console.log(data.length);
+				if (data.length != 0) {
+					var crimeData = processCrimeLoc(data);
+					var pointArray = new google.maps.MVCArray(crimeData);
+					if (self.heatmap) {
+						self.heatmap.setData(pointArray);
+					} else {
+						self.heatmap = new google.maps.visualization.HeatmapLayer({
+	    					data: pointArray
+						});
+					} ;
+					// self.heatmap.setMap(self.map);
+				} else {
+					$info.css('display', 'block');
+				};
+			},
+			error: function(err) { alert(err) ;},
+			beforeSend: function(xhr) {
+				xhr.setRequestHeader("X-Mashape-Authorization", "KqEmEEqnUomsh36Fd17Aei0l9fGqp1BTzhCjsnMPd4iZC218H6");
+			}
+		});
+	};
+
+	this.showCrimeMap = function(coordinate) {
+		$.ajax({
+			url: 'https://jgentes-Crime-Data-v1.p.mashape.com/crime?enddate='+this.endDate+'&lat='+coordinate.lat()+'&long='+coordinate.lng()+'&startdate='+this.startDate,
+			type: 'GET',
+			dataType: 'json',
+			success: function(data) {
+				if (data.length != 0) {
+					var crimeData = processCrimeLoc(data);
+					var pointArray = new google.maps.MVCArray(crimeData);
+					self.heatmap = new google.maps.visualization.HeatmapLayer({
+	    				data: pointArray
+					});
+					// self.heatmap.setMap(self.map);
+				} else {
+					$info.css('display', 'block');
+				};
+			},
+			error: function(err) { alert(err) ;},
+			beforeSend: function(xhr) {
+				xhr.setRequestHeader("X-Mashape-Authorization", "KqEmEEqnUomsh36Fd17Aei0l9fGqp1BTzhCjsnMPd4iZC218H6");
+			}
+		});
+	};
+
+	this.toggleCrimeMap = function() {
+		if (self.heatmap) {
+			self.heatmap.setMap(self.heatmap.getMap() ? null : self.map)
+		} else {
+			self.testCrimeMap(self.coordinate);
+		};
+
+	};
+
+	// Get new center after moving map and request new crime data
+	// google.maps.event.addListener(self.map, 'center_changed', function() {
+
+	// 	// self.showCrimeMap(self.map.getCenter);
+	// });
+
+};
+
+$(document).ready(function() {
+	ko.applyBindings(new mapViewModel);
 });
