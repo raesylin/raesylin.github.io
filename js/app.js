db96b780697a79b897c89a814db79178e764c39a
raesylin <sylin_gibu@hotmail.com>
2016-07-26 12:01:24 -0700

    upload customized bootstrap css and js; cleaned up

--- js/app.js
+++ js/app.js
@@ -4,10 +4,12 @@
 	$(window).scroll(function() { 
 		// check if scroll event happened
 
-		if ($(document).scrollTop()> 50) {
+		if ($(document).scrollTop()> 50) { 
+		// if scrolled more than 50 px from the top of browser window
 			$('.navbar-wrapper nav').removeClass('navbar-default').addClass('navbar-inverse');
 
-		} else {
+		} else { 
+			// if back to the top of the page, restore original class
 			$('.navbar-wrapper nav').removeClass('navbar-inverse').addClass('navbar-default');
 		}
 
